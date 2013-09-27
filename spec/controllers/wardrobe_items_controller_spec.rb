f0a4c51776544f7859a0b1d22d448d1722b62234
Matt Buck <github@matthewbuck.com>
2013-09-27 15:14:45 -0500

    Add failing spec for show action

--- spec/controllers/wardrobe_items_controller_spec.rb
+++ spec/controllers/wardrobe_items_controller_spec.rb
@@ -8,4 +8,12 @@ describe WardrobeItemsController do
       expect(assigns('wardrobe_items')).to eq([wardrobe_item])
     end
   end
+
+  describe 'GET show' do
+    it "assigns the requested wardrobe item as @wardrobe_item" do
+      wardrobe_item = WardrobeItem.create
+      get :show, {:id => wardrobe_item}
+      expect(assigns('wardrobe_item')).to eq(wardrobe_item)
+    end
+  end
 end
