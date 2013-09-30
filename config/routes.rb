95e814f24da8edfbd1309db78f12162e75d38e55
veleary <veleary@gmail.com>
2013-09-30 10:22:58 -0500

    Add view page index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LionWitch::Application.routes.draw do
+  resources :wardrobe_items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
