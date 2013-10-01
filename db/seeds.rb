c5f1baab44299aeaa14fa835963290df0ebec21a
Matt Buck <github@matthewbuck.com>
2013-10-01 01:23:36 -0500

    Add seeds and index/show views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+WardrobeItem.create garment: 'dress',  label: 'Michael Kors',  season: 'spring', color: 'red'
+WardrobeItem.create garment: 'shoes',  label: 'Steve Madden',  season: 'winter', color: 'black'
+WardrobeItem.create garment: 'blouse', label: 'Eileen Fisher', season: 'spring', color: 'blue'
